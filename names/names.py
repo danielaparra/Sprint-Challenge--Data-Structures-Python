06db5c346b9f591360a2130eb2d64d0bebf62321
Daniela Parra <dyparra47@gmail.com>
2019-03-01 12:29:34 -0500

    Use data structure of hash table for names

--- names/names.py
+++ names/names.py
@@ -1,6 +1,6 @@
 import time
 
-start_time = time.time()
+start_time = time.time()ß
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -10,14 +10,26 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
 # This method currently has 0(n^2) time complexity.
+# duplicates = []
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
+# This new method has 0(2n) -> 0(n) big o time complexity.
+# Initalize a dictionary and add all names from names 1 list.
+dictionary = dict()
+for name in names_1:
+    dictionary[name] = name
 
+# Initialize duplicates.
+duplicates = []
+# Check if the dict contains a name from names 2 list.
+for name2 in names_2:
+    # If so, add name to duplicates array.
+    if dictionary.get(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
