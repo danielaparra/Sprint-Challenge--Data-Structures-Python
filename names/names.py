31f1a7ac4c66a4f94ddcebbc92f566b24270225c
Daniela Parra <dyparra47@gmail.com>
2019-03-01 11:58:29 -0500

    Add binary search tree

--- names/names.py
+++ names/names.py
@@ -11,10 +11,13 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# This method currently has 0(n^2) time complexity.
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
